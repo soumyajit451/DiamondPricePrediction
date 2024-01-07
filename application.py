78e75744892b71db104c42b7d7ad6ff1e6924b67
soumyajit <soumyajitsahu2013rta@gmail.com>
2024-01-07 21:07:20 +0530

    css added changes

--- application.py
+++ application.py
@@ -13,29 +13,34 @@ def home_page():
 @app.route('/predict',methods=['GET','POST'])
 
 def predict_datapoint():
-    if request.method=='GET':
-        return render_template('form.html')
-    
-    else:
-        data=CustomData(
-            carat=float(request.form.get('carat')),
-            depth = float(request.form.get('depth')),
-            table = float(request.form.get('table')),
-            x = float(request.form.get('x')),
-            y = float(request.form.get('y')),
-            z = float(request.form.get('z')),
-            cut = request.form.get('cut'),
-            color= request.form.get('color'),
-            clarity = request.form.get('clarity')
-        )
-        final_new_data=data.get_data_as_dataframe()
-        predict_pipeline=PredictPipeline()
-        pred=predict_pipeline.predict(final_new_data)
 
-        results=round(pred[0],2)
+    try:
+         
+        if request.method=='GET':
+            return render_template('form.html')
+        
+        else:                    
+            data=CustomData(
+                carat=float(request.form.get('carat')),
+                depth = float(request.form.get('depth')),
+                table = float(request.form.get('table')),
+                x = float(request.form.get('x')),
+                y = float(request.form.get('y')),
+                z = float(request.form.get('z')),
+                cut = request.form.get('cut'),
+                color= request.form.get('color'),
+                clarity = request.form.get('clarity')
+            )
+           
+            final_new_data=data.get_data_as_dataframe()
+            predict_pipeline=PredictPipeline()
+            pred=predict_pipeline.predict(final_new_data)
+            search_query = request.form.get('search_query', '')
+            results=round(pred[0],2)         
+            return render_template('form.html',final_result=results, search_query=search_query)
+    except:
 
-        return render_template('form.html',final_result=results)
-    
+        return render_template("form.html", no_value = 1)
 
 if __name__=="__main__":
     app.run(host='0.0.0.0',debug=True)
